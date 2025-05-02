8ca6cadf502e1c74c96992a61e263a604c576717
Alen <alen.veselic01@gmail.com>
2025-05-03 01:06:26 +0200

    Try to make project work again unsuccesfully

--- pythonScripts/jobScraper.py
+++ pythonScripts/jobScraper.py
@@ -1,8 +1,8 @@
 import requests, bs4, json
 
 urls = {
-    "https://slo-tech.com/delo":[".forums tbody tr", ".name", ".company", ".last_msg"]
-    "https://www.mojedelo.com/prosta-delovna-mesta/racunalnistvo-programiranje/vse-regije?p=1":".job-ad"
+    # "https://slo-tech.com/delo": [".forums tbody tr", ".name", ".company", ".last_msg"],
+    "https://www.mojedelo.com/prosta-delovna-mesta/racunalnistvo-programiranje/vse-regije?p=1": ".job-ad",
 }
 
 listing = {}
@@ -12,18 +12,16 @@ for url in urls.keys():
     response = requests.get(url)
     response.raise_for_status()
 
-    soup = bs4.BeautifulSoup(response.text, 'html.parser')
-    
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+
     jobs = soup.select(urls[url])
     jobNum = 0
     for job in jobs:
-        
-        listing[url]["job" + str(jobNum)]=[]
+
+        listing[url]["job" + str(jobNum)] = []
         listing[url]["job" + str(jobNum)].append(job.select_one(".name").text)
         listing[url]["job" + str(jobNum)].append(job.select_one(".company").text)
         listing[url]["job" + str(jobNum)].append(job.select_one(".last_msg").text)
         jobNum = jobNum + 1
-    
 
     print(json.dumps(listing))
-
--- routes/scrape.js
+++ routes/scrape.js
@@ -1,30 +1,33 @@
+const { request } = require('express');
 var express = require('express');
 const { PythonShell } = require('python-shell');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
 
-    let py = new PythonShell("./pythonScripts/jobScraper.py")
-    let jobs = ""
-    let currentJob = []
+    // let py = new PythonShell("./pythonScripts/jobScraper.py")
+    // let jobs = ""
+    // let currentJob = []
 
-    py.on("message", (message) => {
-        jobs = message
-    })
+    // py.on("message", (message) => {
+    //     jobs = message
+    // })
     
-    py.end((err, code, signal) => {
-        if(err) throw err;
+    // py.end((err, code, signal) => {
+    //     if(err) throw err;
 
-        console.log(jobs)
-        res.render('scrapeList',{title : "Jobs from Slotech",jobs : JSON.parse(jobs)});
-        console.log('The exit code was: ' + code);
-        console.log('The exit signal was: ' + signal);
-        console.log('finished');
-    })
+    //     console.log(jobs)
+    //     res.render('scrapeList',{title : "Jobs from Slotech",jobs : JSON.parse(jobs)});
+    //     console.log('The exit code was: ' + code);
+    //     console.log('The exit signal was: ' + signal);
+    //     console.log('finished');
+    // })
+    mojeDeloResponse = await fetch("https://slo-tech.com/delo");
 
-    
-  
+    console.log(mojeDeloResponse.body);
+
+    res.send(mojeDeloResponse.body);
 });
 
 module.exports = router;
